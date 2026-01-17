58dee19c81da6fec68b6ffcbce64b9f8a14858c8
kisinga <stevekisinga@gmail.com>
2026-01-17 19:01:02 +0300

    bug fix

--- edge/pi/postgres/init-db.sql
+++ edge/pi/postgres/init-db.sql
@@ -1,5 +1,5 @@
 -- Initialize databases for ChirpStack and ThingsBoard
--- Note: PostgreSQL 15+ requires explicit schema grants
+-- Note: PostgreSQL 15+ requires explicit schema ownership transfer
 
 -- ChirpStack database
 CREATE DATABASE chirpstack;
@@ -7,11 +7,7 @@ CREATE USER chirpstack WITH PASSWORD 'chirpstack';
 GRANT ALL PRIVILEGES ON DATABASE chirpstack TO chirpstack;
 \c chirpstack
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
-GRANT ALL ON SCHEMA public TO chirpstack;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO chirpstack;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO chirpstack;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO chirpstack;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO chirpstack;
+ALTER SCHEMA public OWNER TO chirpstack;
 
 -- ThingsBoard database
 \c postgres
@@ -19,8 +15,4 @@ CREATE DATABASE thingsboard;
 CREATE USER thingsboard WITH PASSWORD 'thingsboard';
 GRANT ALL PRIVILEGES ON DATABASE thingsboard TO thingsboard;
 \c thingsboard
-GRANT ALL ON SCHEMA public TO thingsboard;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO thingsboard;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO thingsboard;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO thingsboard;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO thingsboard;
+ALTER SCHEMA public OWNER TO thingsboard;
